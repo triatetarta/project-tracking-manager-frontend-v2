6eb1c798bcb22e1ee0b27e69e0caaeeec602e460
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-08-14 13:11:26 +0100

    favicon

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { store } from "./configureStore";
 import { Provider } from "react-redux";
+import Favicon from "react-favicon";
 import "./styles/main.css";
+import "./utils/axiosGlobal";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
+    <div>
+      yo
+      <Favicon url='./public/favicon.ico' />
+    </div>
     <Router>
       <App />
     </Router>
